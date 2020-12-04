9727849c832ee3961aff7e0d3040cbd998111d2e
Clément Albanel <57948666+calbanel@users.noreply.github.com>
2020-12-04 03:23:00 +0100

    correctif

--- src/Form/TestimonialsType.php
+++ src/Form/TestimonialsType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\Testimonials;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\ChoiceList\ChoiceList;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
