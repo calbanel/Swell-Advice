82d9481cad8105fb6a18f1db7f76110651ac8766
Clément Albanel <57948666+calbanel@users.noreply.github.com>
2020-12-04 03:22:11 +0100

    testimonials form

--- src/Form/TestimonialsType.php
+++ src/Form/TestimonialsType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Testimonials;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\ChoiceList\ChoiceList;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
